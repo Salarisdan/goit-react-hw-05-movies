c90e2fac76e773b1f74e74cdb86b4e695ab58157
Salarisdan <salarisdan@gmail.com>
2023-03-20 15:11:41 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,31 @@
-import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import  SharedLayout  from './SharedLayout/SharedLayout';
+import { useEffect,  } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import SharedLayout from './SharedLayout/SharedLayout';
+import HomePage from '../pages/HomePage';
+import MovieDetailsPage from '../pages/MovieDetailsPage';
+import MoviesPage from '../pages/MoviesPage';
+import NotFound from '../pages/NotFound';
+import Cast from './Cast/Cast';
+import Reviews from './Reviews/Reviews';
 
-
-const HomePage = lazy(() => import('../pages/HomePage'));
-const MovieDetailsPage = lazy(() => import('../pages/MovieDetailsPage'));
-const Movies = lazy(() => import('../pages/Movies'));
-const NotFound = lazy(() => import('../pages/NotFound'));
-const Cast = lazy(() => import('./Cast/Cast'));
-const Reviews = lazy(() => import('./Reviews/Reviews'));
-
-export const App = () => {
-
-  // useEffect(() => {
-  //   return () => {
-      
-  //   };
-  // }, []);
+const App = () => {
+  useEffect(() => {
+    return () => {};
+  }, []);
 
   return (
-    <Routes>
+    <Switch>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="movies" element={<Movies />} />
+        <Route path="movies" element={<MoviesPage />} />
         <Route path="movies/:movieId" element={<MovieDetailsPage />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
       </Route>
       <Route path="*" element={<NotFound />} />
-    </Routes>
+    </Switch>
   );
 };
+
+export default App;
