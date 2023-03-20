8e56ecd76cdda5d1a0bb9b93b707495ae67f900f
Salarisdan <110417772+Salarisdan@users.noreply.github.com>
2023-03-20 17:13:26 +0000

    Deploying to gh-pages from @ Salarisdan/goit-react-hw-05-movies@4bf1a54d9fd020aa1603ed1693e7a64b48143eda 🚀

--- static/js/108.e4c85d1b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[108],{108:function(e,t,n){n.r(t);var i=n(184);t.default=function(){return(0,i.jsxs)("main",{style:{textAlign:"center"},children:[(0,i.jsx)("b",{style:{fontSize:64},children:"404"}),(0,i.jsx)("p",{children:"Sorry, we couldn't find that movie :("})]})}}}]);
-//# sourceMappingURL=108.e4c85d1b.chunk.js.map
